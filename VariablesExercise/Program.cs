7b9f213466b14a2007ed2487b1f489ee01c6f89b
Mike Cowell <spike@spikeandbetty.com>
2022-10-13 21:34:22 -0400

    Fan Fiction

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int lifesMeaning = 42;
+            string myDungeon = "Got Dragons";
+            char whereIts = '@';
+            bool myMoustacheIsFresh = true;
+            double pi = 3.14;
+            decimal pie = 31.4m;
+            Console.WriteLine($"Season {lifesMeaning} of Game Of Thrones {myDungeon}, I'm talking {pi} mininum and {pie} maximum. -{whereIts}GRRM.");
+
         }
     }
 }
